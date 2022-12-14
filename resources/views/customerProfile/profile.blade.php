bb88aa67339b531f292be149c9c4bfb3dae8eee4
Massarh <alhassanmassarh@gmail.com>
2022-12-14 10:12:51 +0300

    edit profile

--- resources/views/customerProfile/profile.blade.php
+++ resources/views/customerProfile/profile.blade.php
@@ -1,123 +1,112 @@
 @include('navLayout.navbar')
 @extends('layouts.app')
-
-@section('title') @lang('Profile') @endsection
-
-@section('css')
-<link href="{{ URL::asset('/assets/libs/bootstrap-datepicker/bootstrap-datepicker.min.css') }}" rel="stylesheet"
-    type="text/css">
+@section('title')
+@lang('Profile')
 @endsection
 
 @section('content')
 
 
-<div class="row justify-content-center">
-    <div class="col-10">
-        <div class="card overflow-hidden">
-            <div class="bg-primary bg-soft">
-                <div class="row" style="background-color: #1a1a1a">
-                    <div class="col-7">
-                        <div class="text-primary p-3">
-                            <h5 style="color: #ffffff">Happy to see you <i class="mdi mdi-heart text-white"></i></h5>
-                        </div>
-                    </div>
-                    <div class="col-5 align-self-end">
-                        <img src="{{ URL::asset('/logo/shopping.png') }}" alt="" class="img-fluid">
-                    </div>
-                </div>
+<section class="vh-100" style="background-color: rgb(255, 255, 255);">
+<div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+    <div class="col-md-12 col-xl-4">
+
+        <div class="card" style="border-radius: 15px; border: solid 0px #ffffff;">
+        <div class="card-body text-center" style="border-radius: 15px; border: solid 0px #ffffff;">
+            <div class="mt-3 mb-4">
+            <img src="{{ $user->image ? Storage::url($user->image) : URL::asset('/logo/man.png') }}"
+            alt=""
+                class="rounded-circle img-fluid" style="width: 150px;" />
             </div>
-            <div class="card-body pt-0">
-                <div class="row ">
+            <h4 class="mb-2">{{ $user->name }}</h4>
+            <p class="text-muted mb-4">{{ $user->user_role }}
+            
 
-                    <div class="col-sm-4">
-                        <div class="avatar-md profile-user-wid mb-4">
-                            <img src="{{ $user->image ? Storage::url($user->image) : URL::asset('/logo/man.png') }}"
-                                alt="" class="img-thumbnail rounded-circle">
-                        </div>
-                        <h5 class="font-size-15 text-truncate">{{ $user->name }}</h5>
+            {{-- Edit profile --}}
+            <a href=""  data-bs-toggle="modal"
+            data-bs-target=".update-profile" style="color: #1A1A1A ;text-decoration: none;"><p class="text-muted mb-4" > Edit profile <i class='far fa-edit'></i> </p></a>
 
-                    </div>
 
-                    <div class="col-sm-8">
-                        <div class="pt-4">
+            
 
-                            <div class="row">
-                                <div class="col-6">
-                                    <h5 class="font-size-15">{{$user->order ? $user->order->count() : '0'}}</h5>
-                                    <p class="text-muted mb-0">Order</p>
-                                </div>
-                                <div class="col-6">
-                                    <h5 class="font-size-15">$1245</h5>
-                                    <p class="text-muted mb-0">Revenue</p>
-                                </div>
-                            </div>
-                            <div class="mt-4">
-                                <a href="" class="btn waves-effect waves-light btn-sm" style="background-color: #1a1a1a; color:#ffffff;
-                                border-radius: 4px; --bs-btn-padding-x: 40;" data-bs-toggle="modal"
-                                    data-bs-target=".update-profile">Edit Profile</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div class="mb-4 pb-2" >
+            <a type="button" href="{{route('order')}}"  class="btn btn-rounded btn-lg" style="margin:5px; --bs-btn-padding-x: 59px; background-color:#1A1A1A ; color:#ffffff  ">
+                My orders 
+            </a>
             </div>
-        </div>
-        <!-- end card -->
-        <div class="card">
-            <div class="card-body">
-                <!-- Order -->
-                @if(Auth::check())
-                <div class="ml-2">
-                    <a class="dropdown-item" href="{{route('order')}}"  style="font-weight: 600; font-size:16px">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-seam" viewBox="0 0 16 16">
-                            <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2l-2.218-.887zm3.564 1.426L5.596 5 8 5.961 14.154 3.5l-2.404-.961zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z"/>
-                        </svg>
-                        Orders</a>
-                </div>
-                @endif
-                <!-- Order -->
+            
+            
+            <div class="flex-container" style=" display: flex;
+            justify-content: space-between; " >
+            <div>
+                <p class="mb-2 h5">{{ $user->created_at->toDateString()}}</p>
+                <p class="text-muted mb-0" >joined at</p>
+            </div>
+            <div class="px-3">
+                <p class="mb-2 h5">{{$user->order ? $user->order->count() : '0'}}</p>
+                <p class="text-muted mb-0">Order count</p>
+            </div>
+            
+
+            
+
+            </div>
+
+        
+{{-- user info  --}}
+<div class="flex-container" style="">
+
+    <hr>
+    {{-- name --}}
+        <div class="row " style="margin-top: 30px ; margin-left:auto;margin-right:auto">
+            <div class="col-sm-3">
+            <p class="mb-0"><b>Name :</b></p>
+            </div>
+            <div class="col-sm-9">
+            <p class="text-muted mb-0">{{ $user->name }} </p>
             </div>
         </div>
 
-        <div class="card">
-            <div class="card-body">
-                <h4 class="card-title mb-4">Personal Information</h4>
-
-                <p class="text-muted mb-4">Hi I'm Cynthia Price,has been the industry's standard dummy text To an
-                    English person, it will seem like simplified English, as a skeptical Cambridge.</p>
-                <div class="table-responsive">
-                    <table class="table table-nowrap mb-0">
-                        <tbody>
-                            <tr>
-                                <th scope="row">Name :</th>
-                                <td>{{ $user->name }}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Phone :</th>
-                                <td>{{ $user->phone_number }}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">E-mail :</th>
-                                <td>{{ $user->email }}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Location :</th>
-                                <td>{{ $user->address }}</td>
-                            </tr>
-                            
-                        </tbody>
-                    </table>
+    {{-- Address --}}
+                <div class="row " style=" margin-top: 10px">
+                <div class="col-sm-3">
+                    <p class="mb-0"><b>Address :</b></p>
                 </div>
-                
+                <div class="col-sm-9">
+                    <p class="text-muted mb-0">{{ $user->address}}</p>
+                </div>
+                </div>
+
+    {{-- phone --}}
+                <div class="row " style=" margin-top: 10px">
+                <div class="col-sm-3">
+                    <p class="mb-0"><b>phone :</b></p>
+                </div>
+                <div class="col-sm-9">
+                    <p class="text-muted mb-0">{{ $user->phone_number}}</p>
+                </div>
+                </div>
+
+    {{-- Email --}}
+                <div class="row " style=" margin-top: 10px">
+                <div class="col-sm-3">
+                    <p class="mb-0"><b>E-mail</b></p>
+                </div>
+                <div class="col-sm-9">
+                    <p class="text-muted mb-0">{{ $user->email}}</p>
+                </div>
+                </div>
             </div>
         </div>
-        <!-- end card -->
+        </div>
 
     </div>
-
-    <div class="col-xl-8">
-
-        <!--  Update Profile example -->
-        <div class="modal fade update-profile" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
+    </div>
+</div>
+</section>
+            <!--  Update Profile example -->
+            <div class="modal fade update-profile" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
             aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
@@ -125,40 +114,34 @@
                         <h5 class="modal-title" id="myLargeModalLabel">Edit Profile</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-
                     <div class="modal-body">
                         <form method="POST" action="{{ route('profile.update') }}" enctype="multipart/form-data">
                             @csrf
                             @method('PUT')
-
                             <div class="mb-3">
                                 <label for="name" class="form-label">Username</label>
                                 <input id="name" name="name" type="text"
                                     class="form-control @error('name') is-invalid @enderror" value="{{ $user->name }}"
                                     autofocus placeholder="Enter name" autocomplete="name">
-
                                 @error('name')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
                                 @enderror
                             </div>
-
                             <!-- Phone Number -->
                             <div class="mb-3">
                                 <label for="phone_number" class="form-label">Phone Number</label>
                                 <input id="phone_number" name="phone_number" type="text"
                                     class="form-control @error('phone_number') is-invalid @enderror"
-                                    value="{{ $user->phone_number }}" autofocus placeholder="07********"
+                                    value="{{ $user->phone_number }}" autofocus placeholder="07****"
                                     autocomplete="new-phone_number">
-
                                 @error('phone_number')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
                                 @enderror
                             </div>
-
                             <!-- Address -->
                             <div class="mb-3">
                                 <label for="address" class="form-label">Address</label>
@@ -166,14 +149,12 @@
                                     class="form-control @error('phone') is-invalid @enderror"
                                     value="{{ $user->address }}" autofocus
                                     autocomplete="new-address">
-
                                 @error('address')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
                                 @enderror
                             </div>
-
                             <!-- Image -->
                             <div class="mb-3">
                                 <label for="image">Profile Picture</label>
@@ -182,14 +163,12 @@
                                         class="form-control @error('image') is-invalid @enderror" autofocus>
                                     <label class="input-group-text" for="image">Upload</label>
                                 </div>
-
                                 @error('image')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
                                 @enderror
                             </div>
-
                             <div class="mt-3 d-flex justify-content-center">
                                 <button class="btn" type="submit"
                                     style="background-color: #1a1a1a; color:#ffffff; border-radius: 4px; --bs-btn-padding-x: 50;">Update</button>
@@ -199,53 +178,4 @@
                 </div><!-- /.modal-content -->
             </div><!-- /.modal-dialog -->
         </div><!-- /.modal -->
-
-        @endsection
-        @section('script')
-        <!-- apexcharts -->
-        <script src="{{ URL::asset('/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
-
-        <script src="{{ URL::asset('/assets/libs/bootstrap-datepicker/bootstrap-datepicker.min.js') }}"></script>
-
-        <!-- profile init -->
-        <script src="{{ URL::asset('/assets/js/pages/profile.init.js') }}"></script>
-{{-- 
-        <script>
-            $('#update-profile').on('submit', function(event) {
-        event.preventDefault();
-        var Id = $('#data_id').val();
-        let formData = new FormData(this);
-        $('#emailError').text('');
-        $('#nameError').text('');
-        $('#dobError').text('');
-        $('#avatarError').text('');
-        $.ajax({
-            url: "{{ url('update-profile') }}" + "/" + Id,
-            type: "POST",
-            data: formData,
-            contentType: false,
-            processData: false,
-            success: function(response) {
-                $('#emailError').text('');
-                $('#nameError').text('');
-                $('#dobError').text('');
-                $('#avatarError').text('');
-                if (response.isSuccess == false) {
-                    alert(response.Message);
-                } else if (response.isSuccess == true) {
-                    setTimeout(function() {
-                        window.location.reload();
-                    }, 1000);
-                }
-            },
-            error: function(response) {
-                $('#emailError').text(response.responseJSON.errors.email);
-                $('#nameError').text(response.responseJSON.errors.name);
-                $('#dobError').text(response.responseJSON.errors.dob);
-                $('#avatarError').text(response.responseJSON.errors.avatar);
-            }
-        });
-    });
-        </script> --}}
-
         @endsection
